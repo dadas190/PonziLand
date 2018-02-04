f7aaa008a3a4eb8cec2dc61b527815a2ff8745bd
dadas190 <d.cernak@pobox.sk>
2018-02-04 23:15:33 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # PonziLand
+
+Welcome to this repository
+
+PonziLand was created because it was becoming pretty hard to keep track of all these EthPyramid clones, especially when most of them has no front-end.
+
+Here it is, in all its glory. It has pretty much all the features I wanted, and honestly, I am too lazy to continue the development too much.
+
+Feel free to fix some bugs that you may find, create a pull request and if its good I will upload it to the main site. Feel free to add your name, list of features and a donate link at the bottom of the page.
+
+#### Some features requested by the community:
+
+1. Better design
+
+2. Display ratio of user's tokens / token supply - but the table is pretty thick already, maybe move all the action buttons (buy, sell, withdraw, reinvest, get out) into a single drop-down-column?
+
+3. New web-chat, the current one is crap - something like the current one, so sending messages is and doesn't refresh the page
---
+++ rape.php
@@ -0,0 +1,57 @@
+<?php	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://etherscan.io/find-similiar-contracts?a=0x2Fa0ac498D01632f959D3C18E38f4390B005e200");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$output = curl_exec($ch); 
+	curl_close($ch);
+	
+	$out = fopen("scamlistrip.js", "w");
+	fwrite($out,'var scams=["0x2Fa0ac498D01632f959D3C18E38f4390B005e200"');
+	
+	$minval=9999999;
+	$minadr='';
+	
+	$oldfile=file_get_contents("scamlist.js");
+	
+	while(true){
+		$pos=strpos($output,"Exact [100]");
+		if($pos==false)break;
+		$output=substr($output,$pos);
+		
+		$pos=strpos($output,"/address/0x");
+		if($pos==false)break;
+		$output=substr($output,$pos);
+		
+		$adr=substr($output,9,42);
+		fwrite($out,',');
+		fwrite($out,'"'.$adr.'"');
+		
+		$pos=strpos($output,"<td>");
+		if($pos==false)break;
+		$output=substr($output,$pos);
+		$end=strpos($output," Ether");
+		
+		$value=substr($output,4,$end-4);
+		$value=str_replace('<b>','',$value);
+		$value=str_replace('</b>','',$value);
+		
+		if(strpos($oldfile,$adr)!=false&&floatval($value)<$minval){
+			$minval=floatval($value);
+			$minadr=(string)$adr;
+		}		
+	
+		$output=substr($output,$pos);
+	}
+	fwrite($out,"];");
+	fclose($out);
+	
+	$lastpump = trim(file_get_contents('lastpump'));
+	if(time()-$lastpump>3600){
+		$out = fopen("pump", "w");
+		fwrite($out,$minadr);
+		fwrite($out," ".(time()+3600));
+		fclose($out);
+		
+		file_put_contents("lastpump",time());
+	}
+?>
